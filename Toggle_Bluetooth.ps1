d0fe679ac5e7b333809e427ad3073adcf7229586
larakevin67 <larakevin67@gmail.com>
2021-03-12 19:53:03 -0700

    Add files via upload

---
+++ Toggle_Bluetooth.ps1
@@ -0,0 +1,36 @@
+﻿Set-ExecutionPolicy Bypass -ErrorAction SilentlyContinue -Confirm:$false -Force 
+ function set-Bluetooth {
+
+[CmdletBinding()] Param (
+    [Parameter(Mandatory=$true)][ValidateSet('Off', 'On')][string]$BluetoothStatus
+)
+If ((Get-Service bthserv).Status -eq 'Stopped') { Start-Service bthserv }
+Add-Type -AssemblyName System.Runtime.WindowsRuntime
+$asTaskGeneric = ([System.WindowsRuntimeSystemExtensions].GetMethods() | ? { $_.Name -eq 'AsTask' -and $_.GetParameters().Count -eq 1 -and $_.GetParameters()[0].ParameterType.Name -eq 'IAsyncOperation`1' })[0]
+Function Await($WinRtTask, $ResultType) {
+    $asTask = $asTaskGeneric.MakeGenericMethod($ResultType)
+    $netTask = $asTask.Invoke($null, @($WinRtTask))
+    $netTask.Wait(-1) | Out-Null
+    $netTask.Result
+}
+[Windows.Devices.Radios.Radio,Windows.System.Devices,ContentType=WindowsRuntime] | Out-Null
+[Windows.Devices.Radios.RadioAccessStatus,Windows.System.Devices,ContentType=WindowsRuntime] | Out-Null
+Await ([Windows.Devices.Radios.Radio]::RequestAccessAsync()) ([Windows.Devices.Radios.RadioAccessStatus]) | Out-Null
+$radios = Await ([Windows.Devices.Radios.Radio]::GetRadiosAsync()) ([System.Collections.Generic.IReadOnlyList[Windows.Devices.Radios.Radio]])
+$bluetooth = $radios | ? { $_.Kind -eq 'Bluetooth' }
+[Windows.Devices.Radios.RadioState,Windows.System.Devices,ContentType=WindowsRuntime] | Out-Null
+Await ($bluetooth.SetStateAsync($BluetoothStatus)) ([Windows.Devices.Radios.RadioAccessStatus]) | Out-Null
+}
+
+
+$device = Get-PnpDevice | Where-Object {$_.Class -like "Bluetooth" -and $_.friendlyname -like "LE-Papichulo" } | select status
+$ne = @($device)
+$ti = $ne | Where-Object { $_.status -like "ok" } | select -ExpandProperty status 
+
+if ($ti -eq "OK") {
+    set-Bluetooth Off | Out-Null
+}
+else {
+    set-Bluetooth On  | Out-Null
+    }
+      
